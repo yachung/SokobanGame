901cac13ad143d323c4c7b4f95067e0203b9d7ca
yachung <dbstjd8267@gmail.com>
2024-05-22 14:08:26 +0900

    Sokoban Project 생성

---
+++ SokobanGame/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SokobanGame
+{
+    // 게임 클래스.
+    // 게임을 관리하고 실행하는 책임을 담당.
+    public class Game
+    {
+        // 실행 메소드(인터페이스)
+        public void Run()
+        {
+            // 게임 실행은 무한 루프로
+            // 패턴 - 게임 루프 패턴
+            while (true)
+            {
+                // 입력 처리 (방향키 입력을 처리)
+
+                // 업데이트 (방향키 입력을 적용한 후에 데이터 처리)
+
+                // 그리기.
+                Console.WriteLine("그리기");
+            }
+        }
+    }
+}
---
+++ SokobanGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SokobanGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 게임 생성 후 실행
+            Game game = new Game();
+            game.Run();
+        }
+    }
+}
